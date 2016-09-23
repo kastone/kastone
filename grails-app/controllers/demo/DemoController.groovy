a9644c1e68f89fd59b8644576b76d55b05ad1a37
Kathy Stone <Kathy.Stone@erg.com>
2016-09-23 16:46:35 -0400

    Adding responseFormats to the DemoController causes the map values to render as null in the corresponding GSON view.

--- grails-app/controllers/demo/DemoController.groovy
+++ grails-app/controllers/demo/DemoController.groovy
@@ -4,6 +4,9 @@ import org.springframework.http.HttpStatus
 
 class DemoController {
 
+    //This causes the values in the map object to render as null in the GSON view.
+    static responseFormats = ['json']
+
     def index() {
         respond([firstName: 'Jeff', submissionPackageId: 42l], status: HttpStatus.CREATED)
     }
